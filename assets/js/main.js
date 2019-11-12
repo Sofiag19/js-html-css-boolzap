be15526517516b9b53505a8ff730ff9b0efb666b
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-12 12:03:25 +0100

    msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 
+  $(".sendmsg").click(function(){
+
+  var messaggio = $(".message").val();
+
+  var elmentmsg = $("#template .msgsent").clone();
+
+  elmentmsg.find(".testo").text(messaggio);
+
+  $("#now_chat").append(elmentmsg);
+
+  $(".message").val("");
+
+
+  $("#user_chat input").
+  // on focus
+
+  $(".chat").click(function(){
+    
+    $(this).css("background-color", rgb(248, 248, 248));
+    $(this).siblings(".chat").css("background-color", white);
+
+  })
 
 
 
 
-
-
-
-
-
+});
 });
