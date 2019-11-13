ead75e07926027d30bcc9f8401be4671843e0189
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-13 17:17:16 +0100

    funzione base punto due

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,21 +24,17 @@ $(document).ready(function(){
 
   // sullo step 2 si tratterà di gestire una funzione di callback, scatenata da evento di digitazione da tastiera sul input di ricerca. Da li dovrò fare un check su ogni singolo nome contenuto su ogni singolo blocco di contatto, quindi un ciclo su tutti i blocchi contatto presenti in pagina (e qui quindi mi servirà l’“each()“), e per il check essendo su stringhe, mi ricordo che alcuni metodi js che erano per gli array, son presenti anche per le stringhe, come per es. l’“includes()“.
 
-
-  $("#cerca").keypress(function(){
-    var ricerca = $("#cerca").val();
-    $(".name").each(function(){
-      var nome = $(this).text();
-      // console.log(nome);
-      if (nome.includes(ricerca) == false) {
-        $(this).parents(".chat").hide();
-      }
+    $('#cerca').keyup(function(){
+      var ricerca = $(this).val().toLowerCase();
+        $('.name').each(function(){
+          var nome = $(this).text().toLowerCase();
+          if (!nome.indexOf(ricerca)) {
+            $(this).parents(".chat").show();
+          } else {
+            $(this).parents(".chat").hide();
+          }
+        });
     });
-    if (ricerca == 0) {
-      $("#cerca").removeAttr("value");
-    }
-  });
-
 
 
 
