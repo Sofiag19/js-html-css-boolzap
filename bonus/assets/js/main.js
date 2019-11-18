777c4ef9bdcde0f3fae8b97d5da7da2777343add
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-11-18 18:11:01 +0100

    cartella bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -10,21 +10,33 @@ $(document).ready(function(){
       // elmentmsg.find(".testo").text(messaggio);
       var copiaTemplSend = $("#hd-template_sent").html();
       var templReadySend = Handlebars.compile(copiaTemplSend);
-      var createObjSend = {text : messaggio};
+      var createObjSend = {text : messaggio, timemsg : this_time()};
       var createElSend = templReadySend(createObjSend);
       var where = $(".now_chat.play");
       where.append(createElSend);
+      // scroll pagina
+      where.scrollTop(10000);
     }
     $(".message").val("");
 
+    var online = setTimeout(function(){
+      $(".last_access").text("online");
+    }, 200);
+    // clearTimeout(online);
+    var staScrivendo = setInterval(function(){
+      $(".last_access").text("Sta scrivendo...");
+    }, 600);
+    clearTimeout(staScrivendo);
     setTimeout(function(){
       // var ricevuto = $("#template .msgreceived").clone();
       // ricevuto.find(".testo").text("ok");
       var copiaTemplRic = $("#hd-template_received").html();
       var templReadyRic = Handlebars.compile(copiaTemplRic);
-      var createObjRic = {text : "ok"};
+      var createObjRic = {text : "ok", timemsg : this_time()};
       var createElRic = templReadyRic(createObjRic);
       where.append(createElRic);
+      // scroll pagina
+      where.scrollTop(10000);
     }, 1000);
 
   }
@@ -69,6 +81,13 @@ $(document).ready(function(){
     inviaMess();
   });
 
+  // inviomsg con invio ("13")
+  $(".message").on('keypress',function(e) {
+    if(e.which == 13) {
+      inviaMess();
+    }
+  });
+
 
   // ricerca in chats
   $('#cerca').keyup(function(){
@@ -98,6 +117,52 @@ $(document).ready(function(){
     $(this).parents(".icona").hide();
   });
 
+  // modifica input ricerca
+  $("#cerca").click(function(){
+    $(this).removeAttr("placeholder");
+    $("#lente").hide();
+    $("#return").show();
+  })
 
+  $("#return").click(function(){
+    $(this).hide();
+    $("#lente").show();
+    $("#cerca").attr("placeholder","Cerca o inizia una nuova chat");
+  })
+
+  // modifica input Messaggio
+  $("#writing").keyup(function(){
+    var valWriting = $(this).val();
+    var lunghWriting = valWriting.length;
+    console.log(lunghWriting);
+    if (lunghWriting == 0) {
+      $("#writing").attr("placeholder","Scrivi un messaggio");
+      $("#mexvocale").show();
+      $("#invia").hide();
+    } else if (lunghWriting > 0) {
+      $("#writing").removeAttr("placeholder");
+      $("#mexvocale").hide();
+      $("#invia").show();
+    }
+  })
+
+  // TODO: mess sta scrivendo
+
+  // TODO: orario
+  function this_time() {
+   var d = new Date();
+   var m = d.getMinutes();
+   var h = d.getHours();
+
+   if (m < 10) {
+     m = "0" + m;
+   };
+   if (h < 10) {
+     h = "0" + h;
+   }
+   return h + ":" + m;
+  };
+
+  // aggiornare chat con ultimo mess e orario
 
 }); //chiusura document ready
